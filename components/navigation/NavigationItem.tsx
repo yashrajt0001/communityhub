e85aa73a0cadccb3a839ee79a682074dcfc8e18c
Yashraj <yeshrajteli12345@gmail.com>
2024-04-22 09:53:45 +0530

    fix: navigation type error

--- components/navigation/NavigationItem.tsx
+++ components/navigation/NavigationItem.tsx
@@ -25,14 +25,14 @@ export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
         <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
-            params.communityId !== id && "group-hover: h-[20px]",
-            params.communityId === id ? "h-[36px]" : "h-[8px]"
+            params?.communityId !== id && "group-hover: h-[20px]",
+            params?.communityId === id ? "h-[36px]" : "h-[8px]"
           )}
         />
         <div
           className={cn(
             "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
-            params.communityId === id && "text-primary rounded-[16px]"
+            params?.communityId === id && "text-primary rounded-[16px]"
           )}
         >
           <Image fill src={imageUrl} alt="Community" />
