5732b5fc9b189536c494137e82091d6eaf3cf299
Yashraj <yeshrajteli12345@gmail.com>
2024-05-06 22:49:50 +0530

    changed icon of socket indicator

--- components/SocketIndicator.tsx
+++ components/SocketIndicator.tsx
@@ -1,22 +1,16 @@
 "use client";
 
+import { ArrowDownUp } from "lucide-react";
 import { useSocket } from "./providers/socket-provider";
 import { Badge } from "./ui/badge";
 
 export const SocketIndicator = () => {
   const { isConnected } = useSocket();
-
-  if (!isConnected) {
-    return (
-      <Badge variant="outline" className="bg-yellow-600 text-white border-none">
-        Fallback: Polling every 1s
-      </Badge>
-    );
-    }
-    
-    return (
-      <Badge variant="outline" className="bg-emerald-600 text-white border-none">
-        Live: Real time updates
-      </Badge>
-    );
+  return (
+    <ArrowDownUp
+      className={`${
+        isConnected ? "text-green-600" : "text-yellow-600"
+      } border-none`}
+    />
+  );
 };
